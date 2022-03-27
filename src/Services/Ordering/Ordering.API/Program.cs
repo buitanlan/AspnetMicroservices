f4b503914fcc03019636590d5fa2f1271b7b3436
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2022-03-27 22:01:27 +0700

    feat(): init Ordering.API.csproj

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,26 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((_, lc) => lc.WriteTo.Console());
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
